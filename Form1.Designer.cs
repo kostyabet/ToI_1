c11f53eac2e4261efca43d78d8982f413a571e40
Kostya Betenya <betenya.kostya@mail.ru>
2025-02-26 09:23:01 +0300

    fet: complete all

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ToI_1
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,12 +29,233 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.colUpButton = new System.Windows.Forms.Button();
+            this.vizenerGeneratedButton = new System.Windows.Forms.Button();
+            this.shifrButton = new System.Windows.Forms.Button();
+            this.deshifrButton = new System.Windows.Forms.Button();
+            this.keyLabel = new System.Windows.Forms.Label();
+            this.methodNameLabel = new System.Windows.Forms.Label();
+            this.langLabel = new System.Windows.Forms.Label();
+            this.keyTextBox = new System.Windows.Forms.TextBox();
+            this.plainTextLabel = new System.Windows.Forms.Label();
+            this.plaintTextBox = new System.Windows.Forms.TextBox();
+            this.plainFileButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ciperTextBox = new System.Windows.Forms.TextBox();
+            this.cipherFileButton = new System.Windows.Forms.Button();
+            this.infoButton = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // colUpButton
+            // 
+            this.colUpButton.Location = new System.Drawing.Point(0, 0);
+            this.colUpButton.Margin = new System.Windows.Forms.Padding(0);
+            this.colUpButton.Name = "colUpButton";
+            this.colUpButton.Size = new System.Drawing.Size(400, 40);
+            this.colUpButton.TabIndex = 0;
+            this.colUpButton.Text = "Столбцовый улучшеный метод";
+            this.colUpButton.UseVisualStyleBackColor = true;
+            this.colUpButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // vizenerGeneratedButton
+            // 
+            this.vizenerGeneratedButton.Location = new System.Drawing.Point(400, 0);
+            this.vizenerGeneratedButton.Margin = new System.Windows.Forms.Padding(0);
+            this.vizenerGeneratedButton.Name = "vizenerGeneratedButton";
+            this.vizenerGeneratedButton.Size = new System.Drawing.Size(400, 40);
+            this.vizenerGeneratedButton.TabIndex = 1;
+            this.vizenerGeneratedButton.Text = "Виженер, самогенерирующийся ключ";
+            this.vizenerGeneratedButton.UseVisualStyleBackColor = true;
+            this.vizenerGeneratedButton.Click += new System.EventHandler(this.vizenerGeneratedButton_Click);
+            // 
+            // shifrButton
+            // 
+            this.shifrButton.Location = new System.Drawing.Point(12, 279);
+            this.shifrButton.Name = "shifrButton";
+            this.shifrButton.Size = new System.Drawing.Size(350, 30);
+            this.shifrButton.TabIndex = 2;
+            this.shifrButton.Text = "Шифрование";
+            this.shifrButton.UseVisualStyleBackColor = true;
+            this.shifrButton.Click += new System.EventHandler(this.shifrButton_Click);
+            // 
+            // deshifrButton
+            // 
+            this.deshifrButton.Location = new System.Drawing.Point(438, 279);
+            this.deshifrButton.Name = "deshifrButton";
+            this.deshifrButton.Size = new System.Drawing.Size(350, 30);
+            this.deshifrButton.TabIndex = 3;
+            this.deshifrButton.Text = "Дешифрование";
+            this.deshifrButton.UseVisualStyleBackColor = true;
+            this.deshifrButton.Click += new System.EventHandler(this.deshifrButton_Click);
+            // 
+            // keyLabel
+            // 
+            this.keyLabel.AutoSize = true;
+            this.keyLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.keyLabel.Location = new System.Drawing.Point(19, 115);
+            this.keyLabel.Margin = new System.Windows.Forms.Padding(10);
+            this.keyLabel.Name = "keyLabel";
+            this.keyLabel.Size = new System.Drawing.Size(196, 18);
+            this.keyLabel.TabIndex = 4;
+            this.keyLabel.Text = "Введите ключ для работы:";
+            // 
+            // methodNameLabel
+            // 
+            this.methodNameLabel.AutoSize = true;
+            this.methodNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.methodNameLabel.Location = new System.Drawing.Point(255, 60);
+            this.methodNameLabel.Name = "methodNameLabel";
+            this.methodNameLabel.Size = new System.Drawing.Size(0, 20);
+            this.methodNameLabel.TabIndex = 5;
+            // 
+            // langLabel
+            // 
+            this.langLabel.AutoSize = true;
+            this.langLabel.Location = new System.Drawing.Point(731, 118);
+            this.langLabel.Name = "langLabel";
+            this.langLabel.Size = new System.Drawing.Size(38, 13);
+            this.langLabel.TabIndex = 6;
+            this.langLabel.Text = "Язык:";
+            // 
+            // keyTextBox
+            // 
+            this.keyTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.keyTextBox.Location = new System.Drawing.Point(217, 113);
+            this.keyTextBox.Name = "keyTextBox";
+            this.keyTextBox.Size = new System.Drawing.Size(497, 23);
+            this.keyTextBox.TabIndex = 7;
+            // 
+            // plainTextLabel
+            // 
+            this.plainTextLabel.AutoSize = true;
+            this.plainTextLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.plainTextLabel.Location = new System.Drawing.Point(31, 171);
+            this.plainTextLabel.Name = "plainTextLabel";
+            this.plainTextLabel.Size = new System.Drawing.Size(184, 18);
+            this.plainTextLabel.TabIndex = 8;
+            this.plainTextLabel.Text = "Введите исходный текст:";
+            // 
+            // plaintTextBox
+            // 
+            this.plaintTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.plaintTextBox.Location = new System.Drawing.Point(217, 170);
+            this.plaintTextBox.Name = "plaintTextBox";
+            this.plaintTextBox.Size = new System.Drawing.Size(497, 23);
+            this.plaintTextBox.TabIndex = 9;
+            // 
+            // plainFileButton
+            // 
+            this.plainFileButton.Location = new System.Drawing.Point(720, 170);
+            this.plainFileButton.Name = "plainFileButton";
+            this.plainFileButton.Size = new System.Drawing.Size(75, 23);
+            this.plainFileButton.TabIndex = 10;
+            this.plainFileButton.Text = "из файла";
+            this.plainFileButton.UseVisualStyleBackColor = true;
+            this.plainFileButton.Click += new System.EventHandler(this.plainFileButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(92, 222);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 18);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Конечный текст:";
+            // 
+            // ciperTextBox
+            // 
+            this.ciperTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ciperTextBox.Location = new System.Drawing.Point(217, 221);
+            this.ciperTextBox.Name = "ciperTextBox";
+            this.ciperTextBox.ReadOnly = true;
+            this.ciperTextBox.Size = new System.Drawing.Size(497, 23);
+            this.ciperTextBox.TabIndex = 12;
+            // 
+            // cipherFileButton
+            // 
+            this.cipherFileButton.Location = new System.Drawing.Point(720, 221);
+            this.cipherFileButton.Name = "cipherFileButton";
+            this.cipherFileButton.Size = new System.Drawing.Size(75, 23);
+            this.cipherFileButton.TabIndex = 13;
+            this.cipherFileButton.Text = "в файл";
+            this.cipherFileButton.UseVisualStyleBackColor = true;
+            this.cipherFileButton.Click += new System.EventHandler(this.cipherFileButton_Click);
+            // 
+            // infoButton
+            // 
+            this.infoButton.Location = new System.Drawing.Point(572, 250);
+            this.infoButton.Name = "infoButton";
+            this.infoButton.Size = new System.Drawing.Size(223, 23);
+            this.infoButton.TabIndex = 14;
+            this.infoButton.Text = "Посмотреть этапы работы алгоритма";
+            this.infoButton.UseVisualStyleBackColor = true;
+            this.infoButton.Visible = false;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            this.openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 321);
+            this.Controls.Add(this.infoButton);
+            this.Controls.Add(this.cipherFileButton);
+            this.Controls.Add(this.ciperTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.plainFileButton);
+            this.Controls.Add(this.plaintTextBox);
+            this.Controls.Add(this.plainTextLabel);
+            this.Controls.Add(this.keyTextBox);
+            this.Controls.Add(this.langLabel);
+            this.Controls.Add(this.methodNameLabel);
+            this.Controls.Add(this.keyLabel);
+            this.Controls.Add(this.deshifrButton);
+            this.Controls.Add(this.shifrButton);
+            this.Controls.Add(this.vizenerGeneratedButton);
+            this.Controls.Add(this.colUpButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(816, 360);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(816, 360);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ТИ, лаб. раб. 1, Вариант 5, Бетеня К.С.";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button colUpButton;
+        private System.Windows.Forms.Button vizenerGeneratedButton;
+        private System.Windows.Forms.Button shifrButton;
+        private System.Windows.Forms.Button deshifrButton;
+        private System.Windows.Forms.Label keyLabel;
+        private System.Windows.Forms.Label methodNameLabel;
+        private System.Windows.Forms.Label langLabel;
+        private System.Windows.Forms.TextBox keyTextBox;
+        private System.Windows.Forms.Label plainTextLabel;
+        private System.Windows.Forms.TextBox plaintTextBox;
+        private System.Windows.Forms.Button plainFileButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ciperTextBox;
+        private System.Windows.Forms.Button cipherFileButton;
+        private System.Windows.Forms.Button infoButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,429 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
+using System.Xml.Serialization;
+
+enum METHOD
+{
+    COLUMN,
+    VIZENER
+}
+
+enum LANGUAGE
+{
+    RU,
+    EN,
+}
 
 namespace ToI_1
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        private METHOD method { get; set; }
+        private LANGUAGE language { get; set; }
+
+        private delegate string EnDeCription(string key, string text);
+
+        private char[] englishAlphabet =
+        {
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+        };
+
+        private char[] russianAlphabet =
+        {
+            'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М',
+            'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ',
+            'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я'
+        };
+
+        public MainForm()
         {
             InitializeComponent();
+
+            method = METHOD.COLUMN;
+            CheckCurrentMethod(method);
+        }
+
+        private (bool status, string info) CheckIsGoodKey(string key)
+        {
+            if (key.Trim().Length == 0)
+                return (false, "Введите ключ для дальнейшей работы!");
+
+            key = key.ToUpper();
+            string resultKey = "";
+            char[] curLang = language == LANGUAGE.EN ? englishAlphabet : russianAlphabet;
+            foreach (char c in key)
+            {
+                if (curLang.Contains(c))
+                    resultKey += c;
+            }
+            if (resultKey.Length == 0)
+                return (false, "В ключе нет ниодного символа соответствующего алфавита!");
+            return (true, resultKey);
+        }
+
+        private (bool status, string plainText) CheckIsGoodPlain(string text)
+        {
+            if (text.Length == 0)
+                return (false, "Введите исходный текст!");
+            return (true, text);
+        }
+
+        private void CheckCurrentMethod(METHOD current)
+        {
+            if (current == METHOD.COLUMN)
+            {
+                language = LANGUAGE.EN;
+                methodNameLabel.Text = "«Столбцовый метод» улучшенный";
+                colUpButton.Enabled = true;
+            }
+            else
+            {
+                language = LANGUAGE.RU;
+                methodNameLabel.Text = "Виженер, самогенерирующийся ключ";
+                vizenerGeneratedButton.Enabled = true;
+            }
+            langLabel.Text = $"Язык: {language.ToString()}";
+            ResetInputs();
+        }
+
+        private void ResetInputs()
+        {
+            keyTextBox.Text = string.Empty;
+            plaintTextBox.Text = string.Empty;
+            ciperTextBox.Text = string.Empty;
+
+            cipherFileButton.Enabled = false;
+            infoButton.Enabled = false;
+        }
+
+        private void SetInputs()
+        {
+            cipherFileButton.Enabled = true;
+            infoButton.Enabled = true;
+        }
+
+        private string getStringFromFile()
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                bool status;
+                string result;
+                (status, result) = ReadStringFromFile(openFileDialog.FileName);
+                if (status)
+                    return result;
+                MessageBox.Show(result, "Ошибка!");
+            }
+            return string.Empty;
+        }
+
+        private bool setStringInFile(string text)
+        {
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                bool status;
+                string result; 
+                (status, result) = WriteStringInFile(saveFileDialog.FileName, text);
+                MessageBox.Show(result, status ? "Успех" : "Ошибка");
+                return status;
+            }
+            return true;
+        }
+
+        private (bool, string) WriteStringInFile(string fileName, string text)
+        {
+            try
+            {
+                File.WriteAllText(fileName, text);
+                return (true, "Строка успешно записана в файл.");
+            }
+            catch (Exception ex)
+            {
+                return (false, $"Ошибка при записи в файл: {ex.Message}");
+            }
+        }
+
+        private (bool, string) ReadStringFromFile(string filename)
+        {
+            try
+            {
+                string content = File.ReadAllText(filename);
+                bool status;
+                string info;
+                (status, info) = CheckIsGoodPlain(content);
+                if (status)
+                    return (true, content);
+                return (false, "Строка не является корректной!");
+            }
+            catch (Exception ex)
+            {
+                return (false, $"Ошибка при чтении файла: {ex.Message}");
+            }
+        }
+
+        private void MainAlgho(EnDeCription Function)
+        {
+            bool status;
+            // key
+            string key;
+            (status, key) = CheckIsGoodKey(keyTextBox.Text);
+            if (!status)
+            {
+                MessageBox.Show(key, "Ошибка!");
+                return;
+            }
+            // plain text
+            string text;
+            (status, text) = CheckIsGoodPlain(plaintTextBox.Text);
+            if (!status)
+            {
+                MessageBox.Show(text, "Ошибка!");
+                return;
+            }
+            string plainText = BuildCurLangString(text.ToUpper(), (language == LANGUAGE.RU ? russianAlphabet : englishAlphabet));
+            // chiper
+            string cipherText = Function(key, plainText);
+            ciperTextBox.Text = GenerateOutputString(text.ToUpper(), plainText, cipherText);
+            SetInputs();
+        }
+
+        private string GenerateOutputString(string text, string plain, string cipher)
+        {
+            string result = string.Empty;
+
+            int encIndex = 0;
+            int textIndex = 0;
+            while (encIndex < cipher.Length && textIndex < text.Length)
+            {
+                if (text[textIndex] != plain[encIndex])
+                {
+                    result += text[textIndex];
+                    textIndex++;
+                }
+                else
+                {
+                    result += cipher[encIndex];
+                    textIndex++;
+                    encIndex++;
+                }
+            }
+
+            return result;
+        }
+
+        private string BuildCurLangString(string text, char[] language)
+        {
+            string result = string.Empty;
+            foreach(char c in text)
+            {
+                if (language.Contains(c))
+                    result += c;
+            }
+            return result;
+        }
+
+        private int GetRows(int[] indexes, string text)
+        {
+            int count = 0;
+            int length = 0;
+            while (length < text.Length)
+            {
+                length += Array.IndexOf(indexes, count + 1) + 1;
+                count++;
+            }
+            return count;
+        }
+
+        private int LastRowCount(int[] indexes, string text)
+        {
+            int count = 0;
+            int length = 0;
+            while (length < text.Length)
+            {
+                length += Array.IndexOf(indexes, count + 1) + 1;
+                count++;
+            }
+            int result = Array.IndexOf(indexes, count) - (length - text.Length);
+            return result;
+        }
+
+        private string ColumnEncription(string key, string text)
+        {
+            // fill first row
+            int[] keyIndexMatrix = new int[key.Length];
+            for (int i = 0; i < key.Length; i++)
+                keyIndexMatrix[i] = Array.IndexOf(englishAlphabet, key[i]) + 1;
+
+            // find ranks
+            var pairs = keyIndexMatrix.Select((value, index) => new { Value = value, Index = index }).ToArray();
+            var sortedPairs = pairs.OrderBy(p => p.Value)
+                                   .ThenBy(p => p.Index)
+                                   .ToArray();
+            int[] ranks = new int[keyIndexMatrix.Length];
+            for (int i = 0; i < sortedPairs.Length; i++)
+            {
+                ranks[sortedPairs[i].Index] = i + 1;
+            }
+            keyIndexMatrix = ranks;
+
+            char[][] matrix = new char[GetRows(keyIndexMatrix, text)][];
+            for (int i = 0; i < matrix.Length; i++)
+                matrix[i] = new char[key.Length];
+
+            // fill matrix
+            int plainTextIndex = 0;
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    matrix[i][j] = text[plainTextIndex++];
+                    if (plainTextIndex == text.Length) break;
+                    if (keyIndexMatrix[j] == i + 1) break;
+                }
+                if (plainTextIndex == text.Length) break;
+            }
+
+            // create result string
+            string result = string.Empty;
+            for (int i = 0; i < matrix[0].Length; i++)
+            {
+                int column = Array.IndexOf(keyIndexMatrix, i + 1);
+                for (int j = 0; j < matrix.Length; j++)
+                    result += matrix[j][column] == '\0' ? "" : matrix   [j][column].ToString();
+            }
+            return result;
+        }
+
+        private string ColumnDecription(string key, string text)
+        {
+            // fill first row
+            int[] keyIndexMatrix = new int[key.Length];
+            for (int i = 0; i < key.Length; i++)
+                keyIndexMatrix[i] = Array.IndexOf(englishAlphabet, key[i]) + 1;
+
+            // find ranks
+            var pairs = keyIndexMatrix.Select((value, index) => new { Value = value, Index = index }).ToArray();
+            var sortedPairs = pairs.OrderBy(p => p.Value)
+                                   .ThenBy(p => p.Index)
+                                   .ToArray();
+            int[] ranks = new int[keyIndexMatrix.Length];
+            for (int i = 0; i < sortedPairs.Length; i++)
+            {
+                ranks[sortedPairs[i].Index] = i + 1;
+            }
+            keyIndexMatrix = ranks;
+
+            char[][] matrix = new char[GetRows(keyIndexMatrix, text)][];
+            for (int i = 0; i < matrix.Length; i++)
+                matrix[i] = new char[key.Length];
+
+            // fill matrix
+            int textIndex = 0;
+            for (int i = 0; i < matrix[0].Length; i++)
+            {
+                int index = Array.IndexOf(keyIndexMatrix, i + 1);
+                for (int j = 0; j < matrix.Length; j++)
+                {
+                    int indexJ = j > keyIndexMatrix.Length ? j + j % keyIndexMatrix.Length : j % keyIndexMatrix.Length;
+                    if (indexJ >= matrix.Length) break;
+
+                    int isLeft = Array.IndexOf(keyIndexMatrix, indexJ + 1);
+                    if (isLeft >= index)
+                        if (j == matrix.Length - 1 && LastRowCount(keyIndexMatrix, text) >= index)
+                            matrix[j][index] = text[textIndex++];
+                        else if (j != matrix.Length - 1)
+                            matrix[j][index] = text[textIndex++];
+                    if (textIndex == text.Length) break;
+                }
+                if (textIndex == text.Length) break;
+            }
+
+            // create result string
+            string result = string.Empty;
+            for (int i = 0; i < matrix.Length; i++)
+                for (int j = 0; j < matrix[i].Length; j++)
+                    result += matrix[i][j] == '\0' ? "" : matrix[i][j].ToString();
+            return result;
+        }
+
+        private string VizinerEncription(string key, string text)
+        {
+            // prepear key
+            if (text.Length > key.Length)
+                key += text.Substring(0, text.Length - key.Length);
+
+            // cyper
+            string result = string.Empty;
+            for (int i = 0; i < text.Length; i++)
+            {
+                int index = (Array.IndexOf(russianAlphabet, text[i]) + Array.IndexOf(russianAlphabet, key[i]) + russianAlphabet.Length) % russianAlphabet.Length;
+                result += russianAlphabet[index];
+            }
+            return result;
+        }
+
+        private string VizinerDecription(string key, string text)
+        {
+            int keyIndex = 0;
+            int resIndex = 0;
+            string result = string.Empty;
+            while (keyIndex < text.Length)
+            {
+                if (keyIndex == key.Length)
+                    key += result[resIndex++];
+                int index = (Array.IndexOf(russianAlphabet, text[keyIndex]) - Array.IndexOf(russianAlphabet, key[keyIndex]) + russianAlphabet.Length) % russianAlphabet.Length;
+                result += russianAlphabet[index];
+                keyIndex++;
+            }
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            method = METHOD.COLUMN;
+            CheckCurrentMethod(method);
+        }
+
+        private void vizenerGeneratedButton_Click(object sender, EventArgs e)
+        {
+            method = METHOD.VIZENER;
+            CheckCurrentMethod(method);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void shifrButton_Click(object sender, EventArgs e)
+        {
+            EnDeCription Function;
+            if (method == METHOD.COLUMN) Function = ColumnEncription;
+            else Function = VizinerEncription;
+            MainAlgho(Function);
+        }
+
+        private void deshifrButton_Click(object sender, EventArgs e)
+        {
+            EnDeCription Function;
+            if (method == METHOD.COLUMN) Function = ColumnDecription;
+            else Function = VizinerDecription;
+            MainAlgho(Function);
+        }
+
+        private void plainFileButton_Click(object sender, EventArgs e)
+        {
+            plaintTextBox.Text = getStringFromFile();
+        }
+
+        private void cipherFileButton_Click(object sender, EventArgs e)
+        {
+            setStringInFile(ciperTextBox.Text);
         }
     }
 }
